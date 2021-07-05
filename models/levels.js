46b85e316712c10c19fa3071efaf08d0679f7d7c
unknown <timhonks.tv@gmail.com>
2021-07-05 14:06:19 -0400

    Fix attempt for levels not working

--- models/levels.js
+++ models/levels.js
@@ -3,7 +3,7 @@ const levelSchema = require('../schemas/level-schema')
 
 //xp needed & add xp
 const getNeededXp = level => 4 * Math.pow(level, 2) + (45 * level) + 120 
-const addXP = async (guildId, userId, xpToAdd, message) => {
+const addXP = async (guildId, userId, xpToAdd, messageOrVoiceState) => {
     await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
         useFindAndModify: false,
@@ -16,14 +16,14 @@ const addXP = async (guildId, userId, xpToAdd, message) => {
                 { upsert: true, new: true })
 
             let { xp, level } = result;
-            const { member, guild } = message;
+            const { member, guild } = messageOrVoiceState;
             const needed = getNeededXp(level);
             console.log(`guild: ${guild.name}, user: ${member.user.tag}, level: ${level}, xp: ${xp}`);
 
             if (xp >= needed) {
                 ++level
                 xp -= needed
-                message.channel.send(`${member} has now leveled up!`, {
+                messageOrVoiceState.member.send(`You have now leveled up!`, {
                     embed: {
                         author: {
                             name: `${member.user.tag}, congrats on ranking up!`,
@@ -33,6 +33,9 @@ const addXP = async (guildId, userId, xpToAdd, message) => {
                         description: `XP needed: ${needed} xp`,
                         thumbnail: {
                             url: member.user.avatarURL({ dynamic: true })
+                        },
+                        footer: {
+                            text: `if you want to stop receiving these, block me.`
                         }
                     }
                 })
@@ -50,15 +53,34 @@ const addXP = async (guildId, userId, xpToAdd, message) => {
 }  
 
 const onCooldown = new Set();
-const onCooldownVoice = new Set();
 module.exports.addXP = addXP;
 
-module.exports = (client, Discord) => {
+module.exports = (client) => {
+
+    client.on('voiceStateUpdate', async (oldState, newState) => {
+        const { guild, member } = oldState;
+        if (member.user.bot) return;
+        if (newState.member.voice.channel) {
+            await mongoose.connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true
+            }).then(() => {
+                try {
+                    setInterval(() => {
+                        addXP(guild.id, member.id, 5, oldState);
+                    }, 60 * 1000)
+                } catch (err) {
+                    console.log(err)
+                }
+            })
+        }
+    })
 
     client.on('message', async message => {
-        const { guild, member } = message;
-        if (message.author.bot) return;
-        if (message.channel.type === "dm") return;
+        const { guild, member, author, channel } = message;
+        if (author.bot) return;
+        if (channel.type === "dm") return;
 
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
@@ -66,83 +88,78 @@ module.exports = (client, Discord) => {
             useUnifiedTopology: true
         }).then(async () => {
             try {
-                const result = await levelSchema.findOne({ guildId: guild.id, userId: member.id });
+                if (!onCooldown.has(author.id)) {
+                    /**
+                     * Adds a random amount of xp between 
+                     * the minimum and maximum.
+                     * @param {Number} min 
+                     * @param {Number} max 
+                     * @returns {Number}
+                     */
+                    function random(min, max) {
+                        min = Math.ceil(min);
+                        max = Math.floor(max);
+                        return Math.floor(Math.random() * (max - min) + min);
+                    }
+                    addXP(guild.id, author.id, random(10, 25), message)
+                    onCooldown.add(author.id)
+                    setTimeout(() => onCooldown.delete(author.id), 20 * 1000)
+                }
+                const result = await levelSchema.findOne({ guildId: guild.id, userId: author.id });
                 if (result === null) return;
                 const { level } = result;
 
                 //FOR MY GUILD ONLY
-                if (message.guild.id === '778461267999588363') {
+                if (guild.id === '778461267999588363') {
                     if (level >= 30) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.add('853840453748654121');
                         })
                     } else if (level < 30) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.remove('853840453748654121');
                         })
                     }
 
                     if (level >= 25) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.add('853840133899026432');
                         })
                     } else if (level < 25) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.remove('853840133899026432');
                         })
                     }
 
                     if (level >= 15) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.add('847730964619591701');
                         })
                     } else if (level < 15) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.remove('847730964619591701');
                         })
                     }
                     
                     if (level >= 10) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.add('847730653466460160');
                         })
                     } else if (level < 10) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.remove('847730653466460160');
                         })
                     }
 
                     if (level >= 5) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.add('778478893451182140');
                         })
                     } else if (level < 5) {
-                        guild.members.fetch(member.id).then(member => {
+                        await guild.members.fetch(member.id).then(member => {
                             member.roles.remove('778478893451182140');
                         })
                     }
-                }
-
-                if (!onCooldownVoice.has(message.author.id)) {
-                    if (member.voice.channel) {
-                        if (member.voice.channel.id !== '853099601669914624') { //my guild's afk channel
-                            addXP(guild.id, member.id, 5, message);
-                        }
-                        onCooldownVoice.add(message.author.id);
-                        setTimeout(() => onCooldown.delete(message.author.id), 60 * 1000);
-                    }
-                }
-                
-                if (!onCooldown.has(message.author.id)) {
-                    //add xp
-                    const random = (min, max) => {
-                        min = Math.ceil(min);
-                        max = Math.floor(max);
-                        return Math.floor(Math.random() * (max - min) + min);
-                    }
-                    addXP(message.guild.id, message.member.id, random(10, 25), message)
-                    onCooldown.add(message.author.id)
-                    setTimeout(() => onCooldown.delete(message.author.id), 20 * 1000)
                 }
             } catch (err) {
                 console.log(err)
